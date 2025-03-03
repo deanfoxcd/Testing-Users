a3b10e59a0abd7434a2b9ca10cc85b117f0f44ff
Dean Fox <deanfoxcd@gmail.com>
2025-03-03 18:19:33 -0500

    add first 2 tests

---
+++ src/components/UserForm.test.js
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import UserForm from './UserForm';
+
+test('it shows 2 inputs and a button', () => {
+  // Render component
+  render(<UserForm />);
+
+  // Manipulate the component or find an element in it
+  const inputs = screen.getAllByRole('textbox');
+  const button = screen.getByRole('button');
+
+  // Assertion - make sure it's doing what we want it to do
+  expect(inputs).toHaveLength(2);
+  expect(button).toBeInTheDocument();
+});
+
+test('onUserAdd function gets called when form is submitted', () => {
+  const mock = jest.fn();
+
+  render(<UserForm onUserAdd={mock} />);
+
+  const [nameInput, emailInput] = screen.getAllByRole('textbox');
+
+  user.click(nameInput);
+  user.keyboard('dean');
+  user.click(emailInput);
+  user.keyboard('test@test.com');
+
+  const button = screen.getByRole('button');
+  user.click(button);
+
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith({ name: 'dean', email: 'test@test.com' });
+});
